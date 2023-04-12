2cc8c8db6c728133e0547b50a4742e35e8e56983
Asad <nazirgill1054@gmail.com>
2023-04-12 20:35:51 +0500

    First Commit on github

---
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //Button btn = findViewById(R.layout.button);
+
+
+}
